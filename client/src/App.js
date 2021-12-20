c71ef562391ace4da9da4415f6d5f871e28080ec
NickInslee <nick.inslee@gmail.com>
2021-12-20 02:53:13 -0700

    Update app

--- client/src/App.js
+++ client/src/App.js
@@ -58,7 +58,7 @@ class App extends Component {
           </div>
           <Router>
             <Navbar/>
-            <Cards/>
+            <Cards/> 
           </Router>
         </div>  
       );
